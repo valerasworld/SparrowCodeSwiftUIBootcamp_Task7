770343907ebe145569918fca90e269963f9bf80c
Valery Zazulin <zazulinvalerii@gmail.com>
2024-03-18 12:46:30 +0100

    Initial Commit

---
+++ SparrowCodeSwiftUIBootcamp_Task7/SparrowCodeSwiftUIBootcamp_Task7App.swift
@@ -0,0 +1,17 @@
+//
+//  SparrowCodeSwiftUIBootcamp_Task7App.swift
+//  SparrowCodeSwiftUIBootcamp_Task7
+//
+//  Created by Валерий Зазулин on 18.03.2024.
+//
+
+import SwiftUI
+
+@main
+struct SparrowCodeSwiftUIBootcamp_Task7App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
